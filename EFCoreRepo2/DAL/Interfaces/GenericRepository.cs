d3fbba64490f783ea9d2f891a4c0f1780ad44852
Snizhana Kostiuk <snizhana2812kostyuk@gmail.com>
2024-03-26 22:28:16 +0200

    Add project files.

---
+++ EFCoreRepo2/DAL/Interfaces/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Interfaces;
+
+public abstract class GenericRepository<T> : IRepository<T> where T : class
+{
+    protected readonly MusicContext _musicContext;
+
+    public GenericRepository()
+    {
+        _musicContext = new MusicContext();
+    }
+
+    public void Add(T item)
+    {
+        _musicContext.Set<T>().Add(item);
+        _musicContext.SaveChanges();
+    }
+
+    public virtual List<T> Get(int count)
+    {
+        return _musicContext.Set<T>().Take(count).ToList();
+    }
+
+    public virtual List<T> GetAll()
+    {
+        return _musicContext.Set<T>().ToList();
+    }
+
+    public void Remove(int id)
+    {
+        T item = _musicContext.Set<T>().Find(id) ??
+            throw new ArgumentNullException();
+
+        _musicContext.Set<T>().Remove(item);
+        _musicContext.SaveChanges();
+    }
+
+    public void Update(T item, int id)
+    {
+        T oldItem = _musicContext.Set<T>().Find(id) ??
+            throw new ArgumentNullException();
+
+        oldItem = item;
+        _musicContext.SaveChanges();
+    }
+}
